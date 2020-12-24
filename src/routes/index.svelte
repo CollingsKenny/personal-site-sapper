88996d23dac4adf7b2eb7fea9ffb2dbdc2cb3f2d
Kenny <collingsKenny@gmail.com>
2020-12-24 15:17:00 -0800

    Clean up index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,54 +1,17 @@
-<script>
-	import successkid from 'images/successkid.jpg';
-</script>
-
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
 <main>
-	<h1>Kenny Collings</h1>
-	<p>🌐 web developer - 🐧 linux enthusiast - 💻 computer nerd - 🏞️ outdoorsman</p>
-	<p>Hi, I like to make thing sometimes. Currently testing software at Cepheid.</p>
+  <h1>Kenny Collings</h1>
+  <p>
+    🌐 web developer - 🐧 linux enthusiast - 💻 computer nerd - 🏞️ outdoorsman
+  </p>
+  <p>
+    I like to make things sometimes. Currently testing software at
+    <a href="https://www.cepheid.com/">Cepheid</a>.
+  </p>
 </main>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
