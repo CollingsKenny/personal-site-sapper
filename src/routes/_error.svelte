34bea51ce2253368ce1d326d89ebb97239c7e92b
Kenny <collingsKenny@gmail.com>
2020-12-25 23:30:19 -0800

    Update error page

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,40 +1,39 @@
-<script>
-	export let status;
-	export let error;
-
-	const dev = process.env.NODE_ENV === 'development';
-</script>
-
 <style>
-	h1, p {
-		margin: 0 auto;
-	}
+  .error-page {
+    margin: 0 auto;
 
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .error * {
+    display: inline;
+  }
 </style>
 
+<script>
+  export let status;
+  export let error;
+
+  const dev = process.env.NODE_ENV === "development";
+</script>
+
 <svelte:head>
-	<title>{status}</title>
+  <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
-
-<p>{error.message}</p>
+<div class="error-page">
+  <h2>Oops!</h2>
+  <div class="error">
+    <h1>{status}</h1>
+    <p><em>{error.message}</em></p>
+  </div>
+  <p>
+    If you think you found an error on my website, please
+    <a href="about">contact me</a>.
+  </p>
+</div>
 
 {#if dev && error.stack}
-	<pre>{error.stack}</pre>
+  <pre>{error.stack}</pre>
 {/if}
