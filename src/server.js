af644ef04300caf7360e96fc081ca99a9b5980ab
Kenny <collingsKenny@gmail.com>
2020-12-31 12:50:02 -0800

    Implemented deployment

--- src/server.js
+++ src/server.js
@@ -6,8 +6,9 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+polka()
 	.use(
+		'personal-site',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
